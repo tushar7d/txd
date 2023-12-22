054f6a504e262df8ac752ef2290606c98f77965f
Tushar <41472705+tushar7d@users.noreply.github.com>
2023-12-23 01:58:45 +0530

    changes

--- pages/api/auth.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function handler(_, res) {
-  res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"')
-  res.statusCode = 401
-  res.end(`Auth Required.`)
-}
--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="  h-screen w-screen items-center flex flex-col justify-center">
-      <div className="text-white font-serif text-3xl">
+      <div className="text-white font-serif text-3xl mb-12">
         You need to login to see this case study
       </div>
       <SignIn />
