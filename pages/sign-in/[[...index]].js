690e06e84ff5b9b053bd8c84cdc8571143ed8d4d
Tushar <41472705+tushar7d@users.noreply.github.com>
2023-12-23 01:39:03 +0530

    changes

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -2,7 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="  h-screen w-screen items-center flex justify-center">
+    <div className="  h-screen w-screen items-center flex flex-col justify-center">
+      <div>You need to login to see this case study</div>
       <SignIn />
     </div>
   );
