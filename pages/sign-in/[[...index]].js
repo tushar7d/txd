a238c3f2a9ae07f2634d1960be8199b5657f3e41
Tushar <41472705+tushar7d@users.noreply.github.com>
2024-01-08 14:45:29 +0530

    changes

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -3,8 +3,11 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="  h-screen w-screen items-center flex flex-col justify-center">
-      <div className="text-white font-serif text-3xl mb-12">
-        You need to login to see this case study
+      <div className="text-white font-serif text-center text-3xl mb-4">
+        This case study is locked
+      </div>
+      <div className="text-white/75 text-xl text-center mb-12">
+        Reach out to me to get access
       </div>
       <SignIn />
     </div>
