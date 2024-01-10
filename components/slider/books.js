dbd0eff5d50da14b43e78fc7809d75d4592f7333
Tushar <41472705+tushar7d@users.noreply.github.com>
2024-01-11 02:59:31 +0530

    changes

---
+++ components/slider/books.js
@@ -0,0 +1,72 @@
+import Slider from "react-slick";
+import Image from 'next/image'
+
+let IndiaSlider = ()=>{
+    const settings = {
+        autoplay: true,
+    autoplaySpeed: 3000,
+   
+    infinite: true,
+    cssEase: "linear",
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      };
+    return(
+        <div className="my-6">
+<Slider {...settings} >
+          <div className="px-3">
+           <Image width={178} height={267} src="/books/01.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/02.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/03.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/04.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/05.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/06.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/07.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/08.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/09.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/10.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/11.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/12.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/13.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/14.png" />
+          </div>
+          <div className="px-3">
+          <Image width={178} height={267} src="/books/15.png" />
+          </div>
+          </Slider>
+        </div>
+        
+    )
+}
+
+export default IndiaSlider
---
+++ components/slider/music.js
@@ -0,0 +1,72 @@
+import Slider from "react-slick";
+import Image from 'next/image'
+
+let Music = ()=>{
+    const settings = {
+        autoplay: true,
+    autoplaySpeed: 3000,
+   
+    infinite: true,
+    cssEase: "linear",
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      };
+    return(
+        <div className="my-6">
+<Slider {...settings} >
+          <div className="px-3">
+           <Image width={200} height={200} src="/music/01.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/02.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/03.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/04.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/05.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/06.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/07.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/08.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/09.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/10.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/11.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/12.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/13.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/14.png" />
+          </div>
+          <div className="px-3">
+          <Image width={200} height={200} src="/music/15.png" />
+          </div>
+          </Slider>
+        </div>
+        
+    )
+}
+
+export default Music
